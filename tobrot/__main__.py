692cbc023afe60493342ab4d3ce1deaaf32e49db
Fajar Fitrianto <60049222+fajarFWD@users.noreply.github.com>
2020-02-24 18:17:55 +0700

    Update __main__.py

--- tobrot/__main__.py
+++ tobrot/__main__.py
@@ -54,13 +54,13 @@ if __name__ == "__main__" :
     #
     incoming_message_handler = MessageHandler(
         incoming_message_f,
-        filters=Filters.command(["link"]) & Filters.chat(chats=AUTH_CHANNEL)
+        filters=Filters.command(["muat"]) & Filters.chat(chats=AUTH_CHANNEL)
     )
     app.add_handler(incoming_message_handler)
     #
     incoming_youtube_dl_handler = MessageHandler(
         incoming_youtube_dl_f,
-        filters=Filters.command(["linkyt"]) & Filters.chat(chats=AUTH_CHANNEL)
+        filters=Filters.command(["muatyt"]) & Filters.chat(chats=AUTH_CHANNEL)
     )
     app.add_handler(incoming_youtube_dl_handler)
     #
